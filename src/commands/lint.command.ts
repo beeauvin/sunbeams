0a26bff4cf9aa1adb49a959d6d05eed1d3e67b17
Cassidy Spring <79487947+cassiecascade@users.noreply.github.com>
2023-04-23 11:48:07 -0700

    feat(lint): alpha - provide a command to run linting
    The goal of this command is to eventually be able to run
    on any project that follows conventions and have it just work - no extra
    config or imports necessary.

---
+++ src/commands/lint.command.ts
@@ -0,0 +1,27 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { Command, CommandRunner } from 'nest-commander'
+import { run } from '../utilities'
+
+@Command({
+  name: 'lint [path]',
+  description: 'ALPHA! linting to brighten your day'
+})
+export class LintCommand extends CommandRunner {
+  async run(args: string[]): Promise<void> {
+    const [path] = args
+    
+    const include = path ?? '.'
+    const exclude = '--ignore-pattern "dist"'
+
+    const color = '--color'
+    const config = '--config ./node_modules/sunbeams/.eslintrc.js'
+
+    const command = `eslint ${include} ${exclude} ${color} ${config}`
+    await run(command)
+  }
+}
